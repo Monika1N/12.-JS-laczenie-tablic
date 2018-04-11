4df585652acc657c7d1203feb1f616977dd55ecf
Monika N <monia.niemczyk1@gmail.com>
2018-04-11 12:00:00 +0200

    Add 'if' and 'push'

--- scripts.js
+++ scripts.js
@@ -3,3 +3,13 @@ var maleNames = ["Piotrek", "Marek", "Arek", "Jarek"];
 var allNames = femaleNames.concat(maleNames);
 
 console.log(allNames);
+
+var newName = "Marian";
+
+
+if (allNames.indexOf(newName) === -1) {
+	var addNewName = allNames.push(newName);
+	console.log(addNewName);
+} else {
+	console.log("To imię już istnieje. Dodaj inne imię")
+}
